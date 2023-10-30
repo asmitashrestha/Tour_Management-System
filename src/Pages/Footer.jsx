d3caad466deb139339d0efd47e8b2d37ffcd3055
Your Name <you@example.com>
2023-10-30 20:14:29 +0545

    footer section

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,51 +1,53 @@
 import { Link } from 'react-router-dom'
 import { BsFacebook, BsInstagram } from 'react-icons/bs'
-import { BiLogoGmail,BiLocationPlus } from 'react-icons/bi'
-import { AiOutlineMail ,AiOutlinePhone} from 'react-icons/ai'
+import { BiLogoGmail, BiLocationPlus } from 'react-icons/bi'
+import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai'
 const Footer = () => {
     return (
         <>
-        <div className="footer mt-20">
-            <div className="foots ml-16">
-                <div className="footer-left">
-                    <h1><span className='text-zinc-800 text-2xl font-serif'>Travel</span> <span className='text-blue-700 text-2xl font-serif'>Nepal</span></h1>
-                    <p className='text-justify text-md text-zinc-900'>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Expedita, nam.</p>
-                </div>
-                <div className="footer-left">
-                    <h1>Discover</h1>
-                    <div className="naves">
-                        <Link to='/'>Home</Link>
-                        <Link to='/about'>About</Link>
-                        <Link to='/tour'>Tour</Link>
+            <div className="footer mt-20">
+                <div className="foots ml-16 mt-11">
+                    <div className="footer-left">
+                        <h1><span className='text-zinc-800 text-2xl font-serif'>Travel</span> <span className='text-blue-700 text-2xl font-serif'>Nepal</span></h1>
+                        <p className='text-justify text-md text-zinc-900'>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Expedita, nam.</p>
                     </div>
-                    <div className="icons flex">
-                        <p className='mr-4 '><BsFacebook /></p>
-                        <p className='mr-4'><BsInstagram /></p>
-                        <p className='mr-4'><BiLogoGmail /></p>
-                    </div>
+                    <div className="footer-left ml-20 text-justify">
+                        <h1 className='text-2xl font-bold justify-center '>Discover</h1>
+                        <div className="naves justify-center ml-4 mt-2 ">
+                            <Link to='/'>Home</Link>
+                            <Link to='/about'>About</Link>
+                            <Link to='/tour'>Tour</Link>
+                        </div>
+                        <div className="icons flex mt-4 mb-5">
+                            <p className='mr-4 '><BsFacebook /></p>
+                            <p className='mr-4'><BsInstagram /></p>
+                            <p className='mr-4'><BiLogoGmail /></p>
+                        </div>
 
+                    </div>
+                </div>
+
+                <div className="foots mt-11 mr-20 ">
+                    <div className="footer-right">
+                        <h2 className='text-2xl font-bold'>QuickLinks</h2>
+                        <div className="links ml-4 mt-2">
+                            <Link to='/gallery'>Gallery</Link>
+                            <Link to='login'>Login</Link>
+                            <Link to='/register'>Register</Link>
+                        </div>
+
+
+                    </div>
+                    <div className="footer-right ml-40">
+                        <h2 className='text-2xl font-bold justify-center'>Contact</h2>
+                        <p className='mt-2'> <span className='text-2xl'> <BiLocationPlus /> </span>  <span className=' font-bold relative bottom-1 left-2 right-3'>Address:</span> <span className=' font-semibold relative bottom-1 left-2'>Jawalakhel,Lalitpur</span> </p>
+                        <p className='mt-2'> <span className='text-2xl'> <AiOutlineMail /> </span> <span className=' font-bold relative bottom-1 left-2 right-3'>Email:</span>  <span className=' font-semibold relative bottom-1 left-2'>travelnepal1@gmail.com</span> </p>
+                        <p className='mt-2'><span className='text-2xl'><AiOutlinePhone /></span>  <span className=' font-bold relative bottom-1 left-2 right-3'> Phone:</span> <span className=' font-semibold relative bottom-1 left-2'>+9779837654567</span> </p>
+                    </div>
                 </div>
             </div>
-            <div className="foots">
-                <div className="footer-right">
-                    <h2>Quicks Links</h2>
-                    <Link to='/gallery'>Gallery</Link>
-                    <Link to='login'>Login</Link>
-                    <Link to='/register'>Register</Link>
-                    
-                </div>
-                <div className="footer-right">
-                    <h2>Contact</h2>
-                    <p><BiLocationPlus/> Address: Jawalakhel,Lalitpur</p>
-                    <p><AiOutlineMail/> Email: travelnepal1@gmail.com</p>
-                    <p><AiOutlinePhone/> Phone:+9779837654567 </p>
-                </div>
-            </div>
-        </div>
-        <p>Copyright 2023 design and developed byz</p>
+            <p className='justify-center text-center mt-4 mb-4 text-zinc-900 font-semibold'>Copyright 2023 design and developed by Asmita Shrestha</p>
         </>
-        
-
     )
 }
 
--- src/component/Availablepackage.jsx
+++ src/component/Availablepackage.jsx
@@ -5,100 +5,103 @@ import { Link } from 'react-router-dom'
 
 const Availablepackage = () => {
     return (
-        <div className="contains lg:ml-6  lg:mt-12 grid grid-cols-1 lg:grid lg:grid-cols-3 lg:gap-1">
-            {/* <h1 className=' mt-5 text-2xl bold font-mono justify-center text-center text-blue-800'>Special Packages</h1> */}
-            <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
-                <div className="box p-3">
-                    <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
-                    <div className="mt-7 relative left-3 ">
-                        <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
-                        <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
-                        <div className="flex justify-between">
-                            <Link to='/package' className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white'>View Details</Link>
-                            <p className='mr-7 mt-6'>$120.00</p>
-                        </div>
+        <>
+         <h1 className=' mt-9 text-2xl bold font-serif justify-center text-center text-blue-800'>Special Packages</h1>
+            <div className="contains lg:ml-6  lg:mt-12 grid grid-cols-1 lg:grid lg:grid-cols-3 lg:gap-1">
+                <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
+                    <div className="box p-3">
+                        <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
+                        <div className="mt-7 relative left-3 ">
+                            <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
+                            <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
+                            <div className="flex justify-between">
+                                <Link to='/package' className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white'>View Details</Link>
+                                <p className='mr-7 mt-6'>$120.00</p>
+                            </div>
 
+                        </div>
                     </div>
+
+                </div>
+                <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
+                    <div className="box p-3">
+                        <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
+                        <div className="mt-7 relative left-3 ">
+                            <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
+                            <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
+                            <div className="flex justify-between">
+                                <Link to='/package' className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white'>View Details</Link>
+                                <p className='mr-7 mt-6'>$120.00</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
+                    <div className="box p-3">
+                        <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
+                        <div className="mt-7 relative left-3 ">
+                            <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
+                            <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
+                            <div className="flex justify-between">
+                                <Link to='/package' className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white'>View Details</Link>
+                                <p className='mr-7 mt-6'>$120.00</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
+                    <div className="box p-3">
+                        <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
+                        <div className="mt-7 relative left-3 ">
+                            <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
+                            <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
+                            <div className="flex justify-between">
+                                <Link to='/package' className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white'>View Details</Link>
+                                <p className='mr-7 mt-6'>$120.00</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
+                    <div className="box p-3">
+                        <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
+                        <div className="mt-7 relative left-3 ">
+                            <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
+                            <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
+                            <div className="flex justify-between">
+                                <Link to='/package' className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white'>View Details</Link>
+                                <p className='mr-7 mt-6'>$120.00</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
+                    <div className="box p-3">
+                        <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
+                        <div className="mt-7 relative left-3 ">
+                            <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
+                            <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
+                            <div className="flex justify-between">
+                                <Link to='/package' className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white '>View Details</Link>
+                                <p className='mr-7 mt-6'>$120.00</p>
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
-            <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
-                <div className="box p-3">
-                    <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
-                    <div className="mt-7 relative left-3 ">
-                        <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
-                        <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
-                        <div className="flex justify-between">
-                            <Link to='/package'  className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white'>View Details</Link>
-                            <p className='mr-7 mt-6'>$120.00</p>
-                        </div>
 
-                    </div>
-                </div>
-
-            </div>
-            <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
-                <div className="box p-3">
-                    <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
-                    <div className="mt-7 relative left-3 ">
-                        <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
-                        <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
-                        <div className="flex justify-between">
-                            <Link to='/package'  className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white'>View Details</Link>
-                            <p className='mr-7 mt-6'>$120.00</p>
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-            <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
-                <div className="box p-3">
-                    <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
-                    <div className="mt-7 relative left-3 ">
-                        <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
-                        <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
-                        <div className="flex justify-between">
-                            <Link to='/package'  className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white'>View Details</Link>
-                            <p className='mr-7 mt-6'>$120.00</p>
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-            <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
-                <div className="box p-3">
-                    <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
-                    <div className="mt-7 relative left-3 ">
-                        <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
-                        <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
-                        <div className="flex justify-between">
-                            <Link to='/package'  className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white'>View Details</Link>
-                            <p className='mr-7 mt-6'>$120.00</p>
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-            <div className='container bg-slate-300 relative  right-1 mb-2 rounded-lg  border-black h-30 lg:relative lg:left-5 '>
-                <div className="box p-3">
-                    <img src={img1} alt="img" className='w-80 relative top-2 left-2 cursor-pointer filter grayscale-0 hover:grayscale max-w-lg transition-all duration-300' />
-                    <div className="mt-7 relative left-3 ">
-                        <p className='flex '> <span className='mt-1'><BiLocationPlus /> </span> Lumbini,kapilvastu</p>
-                        <p className='flex'> <span className='mt-1'><BsStarFill /></span> 7 days package</p>
-                        <div className="flex justify-between">
-                            <Link to='/package'  className='bg-blue-900 rounded-md px-3 py-2 mt-4 text-white '>View Details</Link>
-                            <p className='mr-7 mt-6'>$120.00</p>
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
+        </>
 
 
 
